3602852bcd51d75bf0a18476d5b77bd29829d84e
Alexander Pasinski <apasinski@nevada.unr.edu>
2019-03-03 21:32:30 -0800

    Increased turningRates for entities

--- As3/src/Entity381.cpp
+++ As3/src/Entity381.cpp
@@ -73,7 +73,7 @@ Destroyer::Destroyer(Ogre::SceneManager * sm, std::string meshfname,
         : Entity381(sm, meshfname, pos, ident)
 {
     acceleration = 50.0f;
-    turningRate = 2.0f;
+    turningRate = 10.0f;
     maxSpeed = 500.0f;
 }
 
@@ -89,7 +89,7 @@ Carrier::Carrier(Ogre::SceneManager * sm, std::string meshfname,
         : Entity381(sm, meshfname, pos, ident)
 {
     acceleration = 40.0f;
-    turningRate = 2.0f;
+    turningRate = 10.0f;
     maxSpeed = 400.0f;
 }
 Carrier::~Carrier()
@@ -104,7 +104,7 @@ Speedboat::Speedboat(Ogre::SceneManager * sm, std::string meshfname,
         : Entity381(sm, meshfname, pos, ident)
 {
     acceleration = 60.0f;
-    turningRate = 5.0f;
+    turningRate = 25.0f;
     maxSpeed = 300.0f;
 }
 
@@ -120,7 +120,7 @@ Frigate::Frigate(Ogre::SceneManager * sm, std::string meshfname,
         : Entity381(sm, meshfname, pos, ident)
 {
     acceleration = 30.0f;
-    turningRate = 5.0f;
+    turningRate = 25.0f;
     maxSpeed = 150.0f;
 }
 
@@ -136,7 +136,7 @@ Alien::Alien(Ogre::SceneManager * sm, std::string meshfname, Ogre::Vector3 pos,
         : Entity381(sm, meshfname, pos, ident)
 {
     acceleration = 15.0f;
-    turningRate = 9.0f;
+    turningRate = 45.0f;
     maxSpeed = 75.0f;
 }
 
